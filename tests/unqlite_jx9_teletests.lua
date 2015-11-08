6afc32937dd45ff307bae4140674fa0c31377493
hcsturix74 <hcsturix74@gmail.com>
2015-11-08 23:32:34 +0100

    fixed index for a print

--- tests/unqlite_jx9_teletests.lua
+++ tests/unqlite_jx9_teletests.lua
@@ -120,7 +120,7 @@ local jx9_program_json = [==[
 local function custom_test_callback(dataout,datalen,ud)
   print("UnQLite Update Hook:",dataout,datalen,ud)
   -- no assert here, this is a callback and errors are NOT managed (at C-level)
-  local t1,t2 = ud["1"], ud["2"]
+  local t1,t2 = ud[1], ud[2]
   print("Userdata:", t1,t2)
 end
 
